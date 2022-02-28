e81f08ff85c9f107ae63cd008d7b5aefaaa5dd62
bonnie <kusasen@gmail.com>
2022-02-28 18:32:10 +0800

    week6/update

--- week6/vue.config.js
+++ week6/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/weeklyVue/week6/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/weeklyVue/week6/dist/" : "/",
 })
